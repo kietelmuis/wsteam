e8f0f0fbd95d767fafcbea5fe19a26e99be8f206
kietelmuis <omar.hopman9@gmail.com>
2026-02-14 22:05:32 +0100

    feat: add manifesthubapi

---
+++ Data/APIs/ManifestHubApi.cs
@@ -0,0 +1,52 @@
+namespace wsteam.Data.APIs;
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.WebUtilities;
+using SteamKit2;
+
+public class ManifestHubApi
+{
+    private HttpClient httpClient;
+    private readonly string apiKey;
+    private const int maxRetries = 5;
+
+    public ManifestHubApi(HttpClient httpClient)
+    {
+        apiKey = Environment.GetEnvironmentVariable("MANIFEST_API_KEY")
+            ?? throw new Exception("No manifest api key");
+
+        this.httpClient = httpClient;
+    }
+
+    public async Task<DepotManifest?> GetManifestAsync(uint depotId, ulong manifestId)
+    {
+        var query = new Dictionary<string, string?>
+        {
+            ["apikey"] = apiKey,
+            ["depotid"] = depotId.ToString(),
+            ["manifestid"] = manifestId.ToString(),
+        };
+
+        var url = QueryHelpers.AddQueryString(
+            "https://api.manifesthub1.filegear-sg.me/manifest",
+            query
+        );
+
+        for (int attempt = 0; attempt < maxRetries; attempt++)
+        {
+            var response = await httpClient.GetAsync(url);
+            if (!response.IsSuccessStatusCode) continue;
+
+            using var stream = await response.Content.ReadAsStreamAsync();
+            return DepotManifest.Deserialize(stream);
+        }
+
+        Console.WriteLine($"failed to download depot {depotId} after {maxRetries} tries");
+        return null;
+    }
+}
