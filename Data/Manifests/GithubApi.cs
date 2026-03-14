b78b10a6845ba0cd8d3aed8e80469772a0be6db7
kietelmuis <omar.hopman9@gmail.com>
2026-03-14 01:23:17 +0100

    fix: remove batchmanifest interface

--- Data/Manifests/GithubApi.cs
+++ Data/Manifests/GithubApi.cs
@@ -6,48 +6,30 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
 using SteamKit2;
+using wsteam.Models.Github;
 
-public class ManifestResponse
-{
-    public required List<ManifestUrlFile> Tree { get; set; }
-}
-
-public class ManifestUrlFile
-{
-    public string Url { get; set; }
-    public string Path { get; set; }
-}
-
-public class ManifestFile
-{
-    public string Content { get; set; }
-}
-
-public class GithubApi : IBatchManifestApi
+public class GithubApi : IManifestApi
 {
     private HttpClient httpClient;
+    private IMemoryCache memoryCache;
 
-    public GithubApi(HttpClient httpClient)
+    public GithubApi(HttpClient httpClient, IMemoryCache memoryCache)
     {
         httpClient.BaseAddress = new Uri("https://api.github.com/repos/SteamAutoCracks/ManifestHub/");
         httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:148.0) Gecko/20100101 Firefox/148.0");
 
         this.httpClient = httpClient;
+        this.memoryCache = memoryCache;
     }
 
     public async Task<DepotManifest?> GetManifestAsync(uint appId, uint depotId, ulong manifestId)
     {
-        var manifests = await GetManifestsAsync(appId);
-        if (manifests is null)
-            return null;
+        if (memoryCache.Get($"{appId}:{depotId}") is DepotManifest manifest)
+            return manifest;
+        Console.WriteLine($"Cache miss for {appId}:{depotId}:{manifestId}");
 
-        manifests.TryGetValue(depotId, out var manifest);
-        return manifest;
-    }
-
-    public async Task<Dictionary<uint, DepotManifest?>?> GetManifestsAsync(uint appId)
-    {
         Console.WriteLine($"Getting manifests for app {appId}");
         var response = await httpClient.GetAsync($"git/trees/{appId}?recursive=1");
         if (!response.IsSuccessStatusCode)
@@ -61,25 +43,26 @@ public class GithubApi : IBatchManifestApi
 
         Console.WriteLine($"Found {manifestResponse.Tree.Count} manifest files");
 
-        var manifestFiles = manifestResponse.Tree.Select(async m =>
-        {
-            if (!m.Path.Contains("manifest")) return ((uint)0, (DepotManifest?)null);
+        var manifests = await Task.WhenAll(
+            manifestResponse.Tree
+                .Where(m => m.Path.Contains("manifest"))
+                .ToList()
+                .Select(async m =>
+                {
+                    var fileResponse = await httpClient.GetAsync($"https://raw.githubusercontent.com/SteamAutoCracks/ManifestHub/{appId}/{m.Path}");
+                    if (!fileResponse.IsSuccessStatusCode) return null;
 
-            var depotId = uint.Parse(m.Path.Split("_")[0]);
+                    var manifest = DepotManifest.Deserialize(await fileResponse.Content.ReadAsByteArrayAsync());
+                    memoryCache.Set($"{appId}:{manifest.DepotID}", manifest);
 
-            var response = await httpClient.GetAsync($"https://raw.githubusercontent.com/SteamAutoCracks/ManifestHub/{appId}/{m.Path}");
-            if (!response.IsSuccessStatusCode)
-            {
-                Console.WriteLine($"Failed to get manifest file: {response.StatusCode}");
-                return (depotId, (DepotManifest?)null);
-            }
+                    return manifest;
+                })
+        );
 
-            return (depotId, DepotManifest.Deserialize(await response.Content.ReadAsByteArrayAsync()));
-        });
-
-        var results = await Task.WhenAll(manifestFiles);
-        return results
-            .Where(r => r.Item1 != 0)
-            .ToDictionary(r => r.Item1, r => r.Item2);
+        return manifests.FirstOrDefault(m =>
+            m is not null &&
+            m.DepotID == depotId &&
+            m.ManifestGID == manifestId
+        );
     }
 }
